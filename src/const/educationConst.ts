9c1416392f86656fc0c0d3c8cf7fceb6a0b8253b
Thiago-Jeager <santiago16waly@gmail.com>
2025-06-20 21:30:55 -0500

    Sección Educacion, Titulos y Certificaciones

---
+++ src/const/educationConst.ts
@@ -0,0 +1,144 @@
+export const degrees = {
+  degrees: [
+    {
+      title: "Universidad Técnica de Cotopaxi",
+      subtitle: "Ingeniero en Sistemas de Información",
+      logo_path: "UTC_logo.webp",
+      alt_name: "UTC",
+      duration: "2019 - 2024",
+      descriptions: [
+        "⚡ He estudiado los conceptos básicos de Ingeniería de Software, Bases de Datos, Redes de Computadoras, Programación y Desarrollo de Software.",
+        "⚡ Tambien he participado en proyectos relacionados con el desarrollo de aplicaciones web y móviles.",
+        "⚡ He participado en la feria de proyectos de la Universidad con una aplicación móvil de RA ganando el segundo lugar.",
+        "⚡ Fui acreedor de una beca para obtner la certificación B1 en inlges Cambridge.",
+      ],
+      website_link: "https://www.utc.edu.ec/",
+    },
+  ],
+};
+
+export const certifications = {
+  certifications: [
+    {
+      title: "Escuela de Formación Ocupacional",
+      subtitle: "- Parque Eco-Industrial el Obraje, Fundación Alianza Suiza y Embajada de Suiza en el Ecuador",
+      logo_path: "certificacion-obraje.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Parque Eco-Industrial el Obraje, Fundación Alianza Suiza y Embajada de Suiza en el Ecuador",
+      color_code: "#FFFFFF",
+      duration: "72 horas",
+      date: "22 de mayo del 2025"
+    },
+    {
+      title: "Escuela de Formación Técnica",
+      subtitle: "- Parque Eco-Industrial el Obraje y la Universidad Hemisferios",
+      logo_path: "UHE_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Parque Eco-Industrial el Obraje y la Universidad Hemisferios",
+      color_code: "#FFFFFF",
+      duration: "72 horas",
+      date: "26 de abril del 2025"
+    },
+    {
+      title: "Introducción a Ciberseguridad",
+      subtitle: "- CISCO Network Academy",
+      logo_path: "Cisco_NA_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "CISCO Network Academy",
+      color_code: "#FFFFFF",
+      duration: "6 horas",
+      date: "Marzo del 2025"
+    },
+    {
+      title: "Introducción a Cisco Packet Trace",
+      subtitle: "- CISCO Network Academy",
+      logo_path: "Cisco_NA_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "CISCO Network Academy",
+      color_code: "#FFFFFF",
+      duration: "2 horas",
+      date: "Enero del 2025"
+    },
+    {
+      title: "Competencias digitales para profesionales",
+      subtitle: "- Google Skillshop",
+      logo_path: "skillshop_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "GOOGLE SKILLSHOP",
+      color_code: "#FFFFFF",
+      duration: "2 horas",
+      date: "Noviembre del 2024"
+    },
+    {
+      title: "Análisis de Amenazas",
+      subtitle: "- CISCO Network Academy",
+      logo_path: "Cisco_NA_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "CISCO Network Academy",
+      color_code: "#FFFFFF",
+      duration: "6 horas",
+      date: "Mayo del 2024"
+    },
+    {
+      title: "Administración de Ciberseguridad",
+      subtitle: "- CISCO Network Academy",
+      logo_path: "Cisco_NA_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "CISCO Network Academy",
+      color_code: "#FFFFFF",
+      duration: "6 horas",
+      date: "Abril del 2024"
+    },
+    {
+      title: "Expositor en la Feria UTCina 2024, ciencia y tecnología al servicio del pueblo",
+      subtitle: "- Universidad Técnica de Cotopaxi",
+      logo_path: "UTC_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Universidad Técnica de Cotopaxi",
+      color_code: "#FFFFFF",
+      duration: "",
+      date: "Febrero del 2024"
+    },
+    {
+      title: "Certificado de Inglés B1",
+      subtitle: "- Cambridge",
+      logo_path: "university-of-cambridge-logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+      alt_name: "Cambridge",
+      color_code: "#FFFFFF",
+      duration: "",
+      date: "Marzo del 2023"
+    },
+    {
+      title: "Mantenimiento Hardware y Software",
+      subtitle: "- Universidad Técnica de Cotopaxi",
+      logo_path: "UTC_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+      alt_name: "Universidad Técnica de Cotopaxi",
+      color_code: "#FFFFFF",
+      duration: "40 horas",
+      date: "Diciembre del 2021"
+    },
+    {
+      title: "Python para ingenieros",
+      subtitle: "- Integcap",
+      logo_path: "integcap_logo.webp",
+      // certificate_link:
+      //   "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+      alt_name: "Integcap",
+      color_code: "#0C9D5899",
+      duration: "4 horas",
+      date: "Octubre del 2021"
+    },
+  ],
+};
