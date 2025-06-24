c08e958edff7842ad5ac3d17e91e8484c235c434
Thiago-Jeager <santiago16waly@gmail.com>
2025-06-23 19:18:14 -0500

    Seccion de Experiencia, componentes, etc

--- src/const/profileConst.ts
+++ src/const/profileConst.ts
@@ -8,5 +8,105 @@ export const profileConst = {
     github: "https://github.com/tuusuario",
     linkedin: "https://linkedin.com/in/tuusuario",
     twitter: "https://twitter.com/tuusuario"
-  }
+  },
+  //Expereincia
+  experienceDesc: "A lo largo de mi formación en Ingeniería de Sistemas y mi participación en diversos proyectos, he desarrollado habilidades para analizar, gestionar y optimizar información, aplicando soluciones tecnológicas estratégicas.",
+  sections: [
+    {
+      title: "Trabajo",
+      work: true,
+      experiences: [
+        {
+          title: "Técnico de digitalización e indexación",
+          company: "Asitecno, GAD del Cantón Mejía",
+          // company_url: "https://www.tiktok.com/en/",
+          logo_path: "mejia_logo.webp",
+          duration: "Mayo 2024 - Diciembre 2024",
+          location: "Mejía, ECU",
+          description:
+            "Digitalización de los libros del Registro de la Propiedad de GAD del Cantón Mejía, indexación de los libros al sistema de base de datos del Registro de la Propiedad para facilitar la consulta de los registros a los empleados e instalación del equipo de trabajo (Computadoras y escáneres) para garantizar el correcto flujo de trabajo.",
+            
+          color: "#0E6BA877",
+        },
+      ],
+    },
+    {
+      title: "Proyectos",
+      experiences: [
+        {
+          title: "Aplicacion web de Cacao",
+          company: "UTC",
+          // company_url: "https://www.utc.edu.ec/",
+          logo_path: "UTC_logo.webp",
+          duration: "Abril 2023 - Agosto 2023",
+          location: "Latacunga, ECU",
+          description:
+            "Optimizar y mejorar el desarrollo de una aplicación web para el monitoreo del crecimiento de plantas de cacao para facilitar el estudio a los estudiantes de Agronomía de la Universidad Técnica de Cotopaxi",
+          color: "#0E6BA877",
+        },
+        {
+          title: "App Móvil de RA",	
+          company: "UTC",
+          // company_url: "https://www.delhivery.com/",
+          logo_path: "UTC_logo.webp",
+          duration: "Octubre 2023 - Febrero 2024",
+          location: "Latacunga, ECU",
+          description:
+            "Desarrollo de una aplicación móvil de realidad aumentada para ilustrar de forma interactiva la flora y fauna del parque nacional Cotopaxi, ganador del segundo puesto en la feria de ciencias de la Universidad Técnica de Cotopaxi.",
+          color: "#0E6BA877",
+        },
+        {
+          title: "Creación de Dashboard Bomberos Mejía",
+          company: "UTC",
+          // company_url:
+          //    "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
+          logo_path: "UTC_logo.webp",
+          duration: "Abril 2023 - Agosto 2023",
+          location: "Latacunga, ECU",
+          description:
+            "Creación de Dashboard usando el programa Power BI para mostrar, optimizar y facilitar los datos generados por la página de bomberos del GAD de Mejía.",
+          color: "#0E6BA877",
+        },
+      ],
+    },
+    {
+      title: "FreeLancer",
+      experiences: [
+        {
+          title: "Mantenimiento de equipos",
+          company: "Manejo de Equipos, Mantenimiento y Reparación, Independiente, freelancer",
+          // company_url: "https://about.google/",
+          logo_path: "mantenimiento_logo.webp",
+          duration: "Marzo 2024 - Presente",
+          location: "Mejía, ECU",
+          description:
+            "Mantenimiento preventivo y correctivo de equipos de cómputo para garantizar su correcto funcionamiento, instalación y configuración de SO y software para uso cotidiano de los clientes e instalación de Hardware y conexión en red de equipos.",
+          color: "#0E6BA877",
+        },
+        {
+          title: "Front End Developer",
+          company: "Freelancer, front end developer, web developer, desarrollador web",
+          // company_url: "https://www.microsoft.com/",
+          logo_path: "frontend_logo.webp",
+          duration: "Mayo 2024 - Presente",
+          location: "Mejía, ECU",
+          description:
+            "He participado en proyectos de desarrollo front end de aplicaciones web utilizando tecnologías como HTML, CSS, JavaScript, React, NodeJS y CodeIgniter.",
+          color: "#0E6BA877",
+        },
+        {
+          title: "Back End Developer",
+          company: "Freelancer, back end developer, desarrollador back end, web developer, desarrollador web, independiente",
+          // company_url: "https://www.mozilla.org/",
+          logo_path: "backend_logo.webp",
+          duration: "Marzo 2024 - Presente",
+          location: "Mejía, ECU",
+          description:
+            "He participado en proyectos de desarrollo back end de aplicaciones web utilizando tecnologías como Python, PHP, SQL y NoSQL, así como el manejo control de versiones con Git.",
+          color: "#0E6BA877",
+        },
+      ],
+    },
+  ],
+
 };
