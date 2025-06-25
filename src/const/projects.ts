87cd6e5ebccf28f1444fe5e39253a55547c17036
Thiago-Jeager <santiago16waly@gmail.com>
2025-06-24 19:58:27 -0500

    Sección de Proyectos, github-readme-stats anuraghazra

---
+++ src/const/projects.ts
@@ -0,0 +1,27 @@
+
+export const project = {
+  githubLink:"https://github.com/Thiago-Jeager",
+  githubStatus:"https://github-readme-stats.vercel.app/api/top-langs/?username=Thiago-Jeager&layout=compact&title_color=001C55&text_color=7F8DAA&border_color=0E6BA877&bg_color=EDF9FE",
+
+  description:"Mis proyectos hacen uso de una gran variedad de herramientas de última tecnología. Mi mejor experiencia es crear proyectos de desarrollo web usando herramientas modernas y haciendo uso de los mejores patrones de desarrollo.",
+  data: [
+    {
+      name:"Portafolio-Thiago",
+      repository: "https://github.com/Thiago-Jeager/Portafolio-Thiago",
+      link:"https://github-readme-stats.vercel.app/api/pin/?username=Thiago-Jeager&repo=Portafolio-Thiago&title_color=001C55&text_color=7F8DAA&border_color=0E6BA877&bg_color=EDF9FE",
+      alt:"Portafolio Thiago Jeager",
+    },
+    {
+      name:"Gestor-Leche",
+      repository: "https://github.com/Thiago-Jeager/Portafolio-Thiago",
+      link:"https://github-readme-stats.vercel.app/api/pin/?username=Thiago-Jeager&repo=Gestor-Leche&title_color=001C55&text_color=7F8DAA&border_color=0E6BA877&bg_color=EDF9FE",
+      alt:"Gestor Leche",
+    },
+    {
+      name:"Flutter-Crud-SQL-NodeJS",
+      repository: "https://github.com/Thiago-Jeager/Flutter-Crud-SQL-NodeJS",
+      link:"https://github-readme-stats.vercel.app/api/pin/?username=Thiago-Jeager&repo=Flutter-Crud-SQL-NodeJS&title_color=001C55&text_color=7F8DAA&border_color=0E6BA877&bg_color=EDF9FE",
+      alt:"Flutter Crud SQL NodeJS",
+    },
+  ],
+};
